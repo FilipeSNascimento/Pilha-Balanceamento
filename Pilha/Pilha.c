9844cc892b4369f8c23971ae1bd6dea7f636ddb4
Filipe Silva do Nascimento <123flipe.nasc3@gmail.com>
2025-02-18 20:39:18 -0300

    inicializando projeto

---
+++ Pilha/Pilha.c
@@ -0,0 +1,28 @@
+#include "Pilha.h"
+
+// Variáveis
+int pilha[TAMANHO];
+int posicao = 0;
+
+// Funções
+bool estaCheia() {
+    return posicao == TAMANHO;
+}
+
+bool push(int valor) {
+    if (estaCheia()) {
+        return false;
+    }
+    pilha[posicao] = valor;
+    posicao++;
+    return true;
+}
+
+bool pop(int *valor) {
+    if (posicao == 0) {
+        return false;
+    }
+    posicao--;
+    *valor = pilha[posicao];
+    return true;
+}
---
+++ Pilha/main.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+#include "Pilha.h"
+
+
+int menu();
